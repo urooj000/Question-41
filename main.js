f00ec4db3c93e06c3bb7185206ee343b82d6cc0a
urooj22 <160038688+urooj000@users.noreply.github.com>
2024-04-19 22:31:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+function showMagicians(magicians) {
+    for (const magician of magicians) {
+        console.log(magician);
+    }
+}
+const magicianNames = ["Penn Jillette", "Criss Angel", "Teller"];
+showMagicians(magicianNames);
